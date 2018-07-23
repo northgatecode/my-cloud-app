d6d044bbfa235ac536397ef1b420dc54faebb5c0
Leo Qian <leoq@vmware.com>
2018-07-23 10:50:37 +0800

    init

---
+++ myweb/src/test/java/com/northgatecode/myweb/MywebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.northgatecode.myweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MywebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
